dd3d2d41799efae4d4715b3db21b7239055f5535
programadorKAgencia <programador2.kagencia@gmail.com>
2024-08-06 17:24:02 -0500

    Avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,97 @@
 // import Spline from '@splinetool/react-spline';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import Bienvenida from './pages/Bienvenida';
+// import Bienvenida from './pages/Bienvenida';
 import Orthogard from './pages/Orthogard';
 import Periogard from './pages/Periogard';
 import Spline from '@splinetool/react-spline';
 import backgroundPeriogard from "./img/fondo_cepillo_periogard.png";
+import backgroundsensitive from "./img/background_sensitive_1.jpg";
+// import backgroundRegimenPerio from "./img/Sensitive_ciclo_5_Mesa_de_trabajo_1_copia.jpg";
+import cepilloPeriogard from "./img/01_Cepillo_Periogard.png";
+import cremas_sensitive from "./img/cremas_sensitive.png";
+import info_sensitive from "./img/info-sensitive.png";
+import periogard_Textos from "./img/periogard_Textos.png";
+import periogard_fondo from "./img/periogard_fondo.jpg";
+import periogard_periogard from "./img/periogard_periogard.png";
 // import React from 'react';
 
 const App = () => {
   return (
     <div className='App'>
 
-      <Parallax pages={5}>
-        <ParallaxLayer offset={0} style={{ backgroundColor: '#f0f0f0' }} factor={1.5}>
-          <h1>HOLAAA</h1>
-          <h1>HOLAAA</h1>
-          <h1>HOLAAA</h1>
-          <h1>HOLAAA</h1>
-          <h1>HOLAAA</h1>
-          <h1>HOLAAA</h1>
-          <h1>HOLAAA</h1>
+      <Parallax pages={6}>
+        <ParallaxLayer offset={0} style={{ backgroundColor: '#d0dedf' }} factor={1}>
+          <Spline className='animation-3d' scene="https://prod.spline.design/k7tZm6s9C6msV3og/scene.splinecode" />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1} factor={1.1}>
+        <ParallaxLayer offset={0} factor={1}>
+
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} factor={1}>
           <Spline className='animation-3d' scene="https://prod.spline.design/QNd4Pk5Vgw1LoPtm/scene.splinecode" />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={1} speed={1.1} factor={1}>
+        <ParallaxLayer offset={1} speed={1} factor={1}>
           <Orthogard />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={2} factor={1.1}>
+        <ParallaxLayer offset={2} factor={1}>
+          <img style={{ height: "100%" }} src={backgroundsensitive} alt="" />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2} factor={1} speed={.4}>
+          <img style={{ height: "100%" }} src={cremas_sensitive} alt="" />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2} factor={1} speed={.9}>
+          <img style={{ height: "100%" }} src={info_sensitive} alt="" />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={3} factor={1}>
+          <img style={{ height: "100%" }} src={periogard_fondo} alt="" />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={3} factor={1} speed={.4}>
+          <img style={{ height: "100%" }} src={periogard_periogard} alt="" />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={3} factor={1} speed={.9}>
+          <img style={{ height: "100%" }} src={periogard_Textos} alt="" />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={4} factor={1}>
           <img src={backgroundPeriogard} alt="" />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={2} speed={1} factor={1}>
+        <ParallaxLayer offset={4} speed={.4} factor={1}>
+          <img
+            style={{
+              width: "50%",
+              margin: "5rem 0 0 23rem"
+              // position: "absolute",
+              // top: 0
+            }}
+            src={cepilloPeriogard} alt="" />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={4} speed={1} factor={1}>
           <Periogard />
         </ParallaxLayer>
 
-        <ParallaxLayer offset={3} speed={1} factor={1.5}>
-          <Bienvenida />
+        <ParallaxLayer offset={5} factor={1}
+        style={{ backgroundColor: '#000' }}
+        >
+          <Spline className='animation-3d' scene="https://prod.spline.design/Y4mZgsq8YAzW5dSh/scene.splinecode" />
         </ParallaxLayer>
+
+        <ParallaxLayer offset={5} factor={1}>
+
+        </ParallaxLayer>
+
+        {/*    <ParallaxLayer offset={6} speed={1} factor={1}>
+          <Bienvenida />
+        </ParallaxLayer> */}
 
 
       </Parallax>
--- src/pages/Orthogard.jsx
+++ src/pages/Orthogard.jsx
@@ -1,68 +1,110 @@
 import Spline from '@splinetool/react-spline';
-import { CgShapeCircle } from "react-icons/cg";
-import '../css/Orthogard.css';
 import { useState } from 'react';
+import imgBullets from "../img/botones-ciclo.svg";
+import '../css/Orthogard.css';
 
 const Orthogard = () => {
 
   const [show, setShow] = useState({
     btn1: false,
     btn2: false,
-    btn3: false
+    btn3: false,
+    btn4: false,
+    ventajas: false
   });
+
   return (
     <div className='Orthogard'>
+      <div className="titulo">
+        <div className='accion'>
+          <h4>Mecanismo</h4>
+          <h1>de ACCIÓN</h1>
+        </div>
+
+        <div className='nombre'>
+          <h4>Enjuague</h4>
+          <h1>Colgate <b>Ortho</b>Gard</h1>
+        </div>
+
+      </div>
       <div className="front">
         <div className='animation'>
           <Spline className='animation-3d' scene="https://prod.spline.design/WmF1T2SGpINjqa74/scene.splinecode" />
         </div>
 
         <div className="information">
-          <h4>Mecanismo</h4>
-          <h1>de ACCIÓN</h1>
-
           <div className="bullets">
-            <div className="btnAcction">
-              <CgShapeCircle className='icon circle' />
-              <button className='btn' onClick={() => setShow({ ...show, btn1: !show.btn1 })}>
-                1
-              </button>
-              {show.btn1 &&
-                <p>
-                  <span className="text-header">Presencia de fluoruro en entorno ácido.</span><br />
-                  <span className="text-body">Reducción de la disolución de la hidroxiapatita.</span>
-                </p>
-              }
+
+            <div className='imgBullets'>
+              <img src={imgBullets} alt="" />
             </div>
 
-            <div className="btnAcction">
-              <CgShapeCircle className='icon circle' />
-              <button className='btn' onClick={() => setShow({ ...show, btn2: !show.btn2 })}>
-                2
-              </button>
-              {show.btn2 &&
-                <p>
-                  <span className="text-header">Aumenta formación de precipitado de CaF2 en período de tiempo corto.</span><br />
-                  <span className="text-body">Menor solubilidad del fluoruro de calcio.</span>
-                </p>
-              }
-            </div>
+            <div>
+              <div className="btnAcction">
+                {/* <CgShapeCircle className='icon circle' /> */}
+                <button className='btn' onClick={() => setShow({ ...show, btn1: !show.btn1 })}>
+                  <span>1</span>
+                </button>
+                {show.btn1 &&
+                  <p>
+                    <span className="text-header">Presencia de fluoruro en entorno ácido.</span><br />
+                  </p>
+                }
+              </div>
 
-            <div className="btnAcction">
-              <CgShapeCircle className='icon circle' />
-              <button className='btn' onClick={() => setShow({ ...show, btn3: !show.btn3 })}>
-                3
-              </button>
-              {show.btn3 &&
-                <p>
-                  <span className="text-header">Favorece formación deluorhidroxiapatita.</span><br />
-                  <span className="text-body">Exposición del esmalte a iones de flúor: captación para formar FAP (desplazamiento del calcio en cristales de HAP).</span>
-                </p>
-              }
+              <div className="btnAcction">
+                {/* <CgShapeCircle className='icon circle' /> */}
+                <button className='btn' onClick={() => setShow({ ...show, btn2: !show.btn2 })}>
+                  <span>2</span>
+                </button>
+                {show.btn2 &&
+                  <p>
+                    <span className="text-header">Reducción de la disolución de la hidroxiapatita.</span><br />
+                  </p>
+                }
+              </div>
+
+              <div className="btnAcction">
+                {/* <CgShapeCircle className='icon circle' /> */}
+                <button className='btn' onClick={() => setShow({ ...show, btn3: !show.btn3 })}>
+                  <span>3</span>
+                </button>
+                {show.btn3 &&
+                  <p>
+                    <span className="text-header">Aumenta formación de precipitado de CaF2 en período de tiempo corto.</span><br />
+                    <span className="text-body">Menor solubilidad del fluoruro de calcio.</span>
+                  </p>
+                }
+              </div>
+
+              <div className="btnAcction">
+                {/* <CgShapeCircle className='icon circle' /> */}
+                <button className='btn' onClick={() => setShow({ ...show, btn4: !show.btn4 })}>
+                  <span>4</span>
+                </button>
+                {show.btn4 &&
+                  <p>
+                    <span className="text-header">Favorece formación deluorhidroxiapatita.</span><br />
+                    <span className="text-body">Exposición del esmalte a iones de flúor: captación para formar FAP (desplazamiento del calcio en cristales de HAP).</span>
+                  </p>
+                }
+              </div>
+
             </div>
           </div>
-          <div className="vl line1"></div>
-          <div className="vl line2"></div>
+
+          <div className="ventajas-div">
+            <div className='content'>
+              <button className='btn ventajas' onClick={() => setShow({ ...show, ventajas: !show.ventajas })}>VENTAJAS</button>
+              <p className="text-header">Indicado para pacientes con alto riesgo de caries dental.</p>
+            </div>
+            {show.ventajas &&
+              <p>
+                <span className="text-header">Resistencia a la disolución en ambiente ácido</span><br />
+                <span className="text-body">Fluoruro actúa como catalizador asistiendo en la remineralización con iones de fosfato</span>
+              </p>
+            }
+          </div>
         </div>
 
       </div>
